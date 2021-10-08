0b3d639808f9345c1e9546f2ce4134777114f0e0
Stepan.Rodionov <Stepan.Rodionov@x5.ru>
2021-10-08 20:34:16 +0300

    Вынесли карточку клиента из index.php

--- www/index.php
+++ www/index.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/requirements.php';
 
-if (!isset($_GET['user_id'])) {
 ?>
 <h1>Клиенты</h1>
 <div>
@@ -49,17 +48,17 @@ foreach ($users as $user) { ?>
 
         <tr>
             <td>
-                <a href="/?user_id=<?= $user['id']?>">
+                <a href="/personal.php?user_id=<?= $user['id']?>">
                     <?= $user['id']?>
                 </a>
             </td>
             <td>
-                <a href="/?user_id=<?= $user['id']?>">
+                <a href="/personal.php?user_id=<?= $user['id']?>">
                     <?= $user['name']?>
                 </a>
             </td>
             <td>
-                <a href="/?user_id=<?= $user['id']?>">
+                <a href="/personal.php?user_id=<?= $user['id']?>">
                     <?= $user['surname']?>
                 </a>
             </td>
@@ -71,44 +70,4 @@ foreach ($users as $user) { ?>
     </tbody>
 </table>
 
-<?php
-} else {
-    $userId = (int)$_GET['user_id'];
-    if ($userId < 1) {
-        die('Некорректный id клиента!');
-    }
-    $user = getUserById($userId);
-?>
-    <div>
-        <h3>Данные клиента <b><?= $user['name'] . ' ' . $user['surname'] ?></b></h3>
-    </div>
-    <table border="1">
-        <tr>
-            <td>Id клиента</td>
-            <td><?= $user['id']?></td>
-        </tr>
-        <tr>
-            <td>Имя</td>
-            <td><?= $user['name']?></td>
-        </tr>
-        <tr>
-            <td>Фамилия</td>
-            <td><?= $user['surname']?></td>
-        </tr>
-        <tr>
-            <td>Телефон</td>
-            <td><?= $user['phone']?></td>
-        </tr>
-        <tr>
-            <td>Почта</td>
-            <td><?= $user['email']?></td>
-        </tr>
-        <tr>
-            <td>Дата создания</td>
-            <td><?= $user['created_at']?></td>
-        </tr>
-    </table>
-    <div>
-        <a href="/">Вернуться к списку</a>
-    </div>
-<?php }
+
